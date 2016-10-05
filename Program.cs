96683fce2332d7f9598854be518408d72089fbd3
berigaj <jenberigan@gmail.com>
2016-10-05 19:45:44 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodingActivity_TicTacToe_ConsoleGame;
+
+namespace CodingActivity_TicTacToe_ConsoleGame
+{
+    class Program
+    {
+        private static int roundsPlayed;
+        private static int playerXWins;
+        private static int playerOWins;
+        private static int catsGames;
+
+        static void Main(string[] args)
+        {
+            
+            GameController gameController = new GameController(roundsPlayed, playerXWins, playerOWins, catsGames);
+           
+        }
+    }
+}
---
+++ ReadMe.txt
@@ -0,0 +1,7 @@
+﻿Title:			Tic-Tac-Toe Game Application
+Description:	This application allows two users playing a 4x4 tic-tac-toe game using the console UI. 
+				The purpose of the application to demonstrate encapsulation and the MVC design pattern.
+Authors:		John E Velis (base code), Jennifer Berigan & Alexandra Briggs (updates and enhancements)
+Dated Created:	6/12/2015
+Last Modified:	9/25/2016
+Instructions for Use:
